dc582132a37503e1f2f7f94d2e751cb8810feaee
GTSorho <you@example.com>
2020-04-21 00:32:33 +0000

    21/04/2020 update

---
+++ app/permanentespdata.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class permanentespdata extends Model
+{
+    public $table = "permanentespdata";
+
+    
+    Protected $fillable = [
+        'user_id',
+        'temperature',
+        'moisture',
+        'light',
+        'humidity',
+        'rain'
+    ];
+}
---
+++ database/migrations/2020_04_20_233707_create_permanentespdatas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermanentespdatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permanentespdatas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedinteger('user_id');
+            $table->double('temperature');
+            $table->double('humidity');
+            $table->double('moisture');
+            $table->double('light');
+            $table->double('rain');        
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permanentespdatas');
+    }
+}
