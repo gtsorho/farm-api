9b70fd07a88ca52ae5fd12c93c2d52fb2c14f44c
GTSorho <you@example.com>
2020-05-01 02:48:18 +0000

    01/05/2020

--- app/Http/Controllers/espcontroller.php
+++ app/Http/Controllers/espcontroller.php
@@ -48,8 +48,6 @@ class espcontroller extends Controller
     {
         
         espdata::create(request()->all() + ['user_id'=>auth()->guard('api')->id()]);
-        permanentespdata::create(request()->all() + ['user_id'=>auth()->guard('api')->id()]);
-
         // $message = espdata::where('user_id', auth()->guard('api')->id())->get();
         $message = espdata::latest('user_id', auth()->guard('api')->id())->first();
         $channelkey = espdata::latest('user_id', auth()->guard('api')->id())->first()->user_id;
@@ -71,17 +69,4 @@ class espcontroller extends Controller
         // $espdata= espdata::find($id);
         return new espdataResource($espdata);
     }
-
-    public function destroy($id) 
-    {
-        $espdata  = espdata::where('user_id', auth()->guard('api')->id())->count();
-        if ( $espdata > 20){
-            // $espdata->destroy();
-            $delete = espdata::where('user_id', auth()->guard('api')->id())->orderBy('id', 'asc')->take(1)->delete();
-            return new espdataResource(["message"=> "deleted"]);
-        }else{
-            return new espdataResource(["message"=> "can't delete"]);
-        }
-    }
-
 }
