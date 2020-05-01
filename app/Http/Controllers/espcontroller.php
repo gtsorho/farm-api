a20e9088e0ea5e92bec699e735167a080b9fecfb
GTSorho <you@example.com>
2020-05-01 10:42:04 +0000

    repairs 30/4/2020

--- app/Http/Controllers/espcontroller.php
+++ app/Http/Controllers/espcontroller.php
@@ -25,7 +25,7 @@ class espcontroller extends Controller
      */
     public function index()
     {
-    //  abort_if($User->id !== auth()->guard('api')->id(), 403);
+ 
      $message = espdata::where('user_id', auth()->guard('api')->id())->get();
 
      $single_data = espdata::latest('user_id', auth()->guard('api')->id())->first();
