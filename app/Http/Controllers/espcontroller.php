e93442b70728cee04558e64662d9abf2392a8920
gtsorho <georgesorho@gmail.com>
2020-08-02 23:31:26 +0000

    query update

--- app/Http/Controllers/espcontroller.php
+++ app/Http/Controllers/espcontroller.php
@@ -9,6 +9,7 @@ use carbon\carbon;
 use App\Events\MyEvent;
 use Illuminate\Http\Request;
 use App\permanentespdata;
+// use Illuminate\Support\Collection;
 use App\Notifications\userNotification;
 use App\Http\Resources\espdata as espdataResource;
 
@@ -32,7 +33,7 @@ class espcontroller extends Controller
     //  abort_if($User->id !== auth()->guard('api')->id(), 403);
     //  $message = espdata::latest('user_id', auth()->guard('api')->id())->orderBy('id', 'desc')->take(20)->get();
      $message = espdata::where('user_id', auth()->guard('api')->id())->orderBy('id','desc')->take(20)->get();
-     $messageRev = array_reverse($message);
+     return $message->reverse();
     
 
      $single_data = espdata::latest('user_id', auth()->guard('api')->id())->first();
@@ -41,9 +42,9 @@ class espcontroller extends Controller
 
     //  $average = espdata::where('created_at', '<', Carbon::now()->subMinutes(5)->toDateTimeString());
 
-     event(new MyEvent(['message'=>$messageRev, 'singleData'=>$single_data, 'status'=>"plural", 'channelkey'=>$channelkey]));
+     event(new MyEvent(['message'=>$message, 'singleData'=>$single_data, 'status'=>"plural", 'channelkey'=>$channelkey]));
 
-     return new espdataResource([$messageRev, $single_data, $channelkey]);
+     return new espdataResource([$message, $single_data, $channelkey]);
     }
 
 
